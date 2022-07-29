3af81a5ed142ec98a30755324f9a0d489084fbec
jdeepee <joshuadparkin@gmail.com>
2022-07-29 12:47:19 +0100

    bump hdk & hdi versions

--- hc-dna/zomes/direct-message-integrity/src/ad4m.rs
+++ hc-dna/zomes/direct-message-integrity/src/ad4m.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, Utc};
-use holochain_deterministic_integrity::prelude::*;
+use hdi::prelude::*;
 
 #[derive(Clone, Debug, Serialize, Deserialize, SerializedBytes)]
 pub struct Link {
